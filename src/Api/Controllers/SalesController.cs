0f8079ba82639e77e6e402be8c212bf9d4017c96
AntonioK <kristicevic.antonio@gmail.com>
2021-01-19 18:20:21 +0100

    Enable XML swagger docs

--- src/Api/Controllers/SalesController.cs
+++ src/Api/Controllers/SalesController.cs
@@ -3,9 +3,13 @@ using Application.Common;
 using Application.Sales.Commands.CreateSale;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+#pragma warning disable 1573
 
 namespace Api.Controllers
 {
+    /// <summary>
+    /// Sale related controller actions. 
+    /// </summary>
     public class SalesController : BaseController
     {
         /// <summary>
@@ -13,16 +17,18 @@ namespace Api.Controllers
         /// </summary>
         /// <remarks>
         /// Sample request:
-        ///     POST /Sales
+        ///
+        ///     POST /sales
         ///     {
-        ///         "articleNumber": "000001",
-        ///         "price": 10.15
+        ///        "articleNumber": "000001",
+        ///        "price": 10.05
         ///     }
+        ///
         /// </remarks>
-        /// <param name="model"></param>
+        /// <param name="model">Create sale request model</param>
         /// <returns>Id of newly created sale</returns>
         /// <response code ="201">Returns the id of newly created sale</response>
-        /// <response code ="400">All properties are required</response>
+        /// <response code ="400">All properties are required. Article number is 32 characters or less. Price has to be a positive value.</response>
         [HttpPost]
         [Produces("application/json")]
         [ProducesResponseType(StatusCodes.Status201Created)]
