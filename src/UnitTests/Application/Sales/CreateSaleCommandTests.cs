dbd8248ac7b8ecf46a8be37632717fc92b87250a
Antonio Kristicevic <kristicevic.antonio@gmail.com>
2021-01-19 16:48:43 +0100

    Implement remaining unit tests

---
+++ src/UnitTests/Application/Sales/CreateSaleCommandTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Sales.Commands.CreateSale;
+using Common.DateTime;
+using Domain.Enums;
+using Infrastructure.Persistence;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTests.Application.Sales
+{
+    [TestFixture]
+    public class CreateSaleCommandTests : BaseTest
+    {
+        private Mock<ILogger<CreateSaleCommand>> _logger;
+        private SalesDbContext _context;
+        private Mock<IDateTimeProvider> _dateTimeProvider;
+
+        private CreateSaleCommand _sut;
+
+        [SetUp]
+        public new void SetUp()
+        {
+            _logger = new Mock<ILogger<CreateSaleCommand>>();
+            _context = new SalesDbContext(ContextOptions);
+            _dateTimeProvider = new Mock<IDateTimeProvider>();
+
+            _sut = new CreateSaleCommand(_logger.Object, _context, _dateTimeProvider.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Dispose();
+        }
+
+        [Test]
+        public async Task Execute_ShouldSaveChangesToContext()
+        {
+            var model = new CreateSaleModel { ArticleNumber = "123456", Price = 10m };
+
+            await _sut.Execute(model);
+
+            Assert.AreEqual(1, _context.Sales.Count());
+            Assert.AreEqual(model.ArticleNumber, _context.Sales.First().ArticleNumber);
+            Assert.AreEqual(model.Price, _context.Sales.First().Price);
+        }
+
+        [Test]
+        public async Task Execute_ShouldSaveSalesAsEuro()
+        {
+            var model = new CreateSaleModel { ArticleNumber = "123456", Price = 10m };
+
+            await _sut.Execute(model);
+
+            Assert.AreEqual(Currency.Euro, _context.Sales.First().Currency);
+        }
+
+        [Test]
+        public async Task Execute_ShouldHaveCorrectSalesDate()
+        {
+            var testDate = new DateTime(2020, 10, 10);
+            _dateTimeProvider.Setup(x => x.UtcNow).Returns(testDate);
+
+            var model = new CreateSaleModel { ArticleNumber = "123456", Price = 10m };
+
+            await _sut.Execute(model);
+
+            Assert.AreEqual(testDate, _context.Sales.First().DateTimeUtc);
+        }
+    }
+}
---
+++ src/UnitTests/Common/DateTimeProviderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Common.DateTime;
+using NUnit.Framework;
+
+namespace UnitTests.Common
+{
+    [TestFixture]
+    public class DateTimeProviderTests
+    {
+        private const int ErrorMarginInMs = 100;
+        private DateTimeProvider _dateTimeProvider;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dateTimeProvider = new DateTimeProvider();
+        }
+
+        [Test]
+        public void UtcNow_ShouldBeCloseToSystemUtcNow()
+        {
+            var systemUtcNow = DateTime.UtcNow;
+            var providerUtcNow = _dateTimeProvider.UtcNow;
+
+            var diff = providerUtcNow - systemUtcNow;
+
+            Assert.LessOrEqual(diff.TotalMilliseconds, ErrorMarginInMs,
+                $"Error margin between system clock and actual clock should be less than {ErrorMarginInMs}");
+        }
+    }
+}
